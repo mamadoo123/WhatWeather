f7594ab81f18c0af36282b365001f286807d2007
mohamed khamis <mklhs66662000@gmail.com>
2019-01-21 21:31:30 +0200

    added the local storage utilities to store data in the Local Storage

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
-// initialize the weather object from the weatherAPI class and ui from UI class
-const weather = new WeatherAPI('London', 'UK');
+//initialize storage object
+const storage = new Storage();
+//get the location data object from the local storage
+const dataFromStorage = storage.getLocation();
+// initialize the weather object with location data stored in the local storage 
+const weather = new WeatherAPI(dataFromStorage.city, dataFromStorage.country);
+//initialize the UI object 
 const ui = new UI();
-// Event Listeners For the DOM Elements
+
+// Event Listener to Load the page
 document.addEventListener('DOMContentLoaded', getWeather);
+// Event Listener to the modal 
 document.getElementById('locationModal').addEventListener('click', changeLocation);
 
+// get the location data from local storage and fetch it to render the weather view 
 function getWeather(){
 weather.getWeather()
-    .then(data => {
-        ui.render(data)})
-    .catch(err => console.log(err));
-
+    .then(data => {ui.render(data)})
+    .catch(() => alert('Enter Correct City Name and Country Code'))
 }
-
+// get the new location data from the input boxes of the modal window
 function changeLocation(e){
     if(e.target.id === 'change-btn'){
+    // get the value of both text inputs
     const cityInput = document.getElementById('city').value;
     const countryInput = document.getElementById('country-code').value;
+    // set the new location data
     weather.changeLocation(cityInput, countryInput);
+    storage.setLocation(cityInput, countryInput);
     getWeather();
+    // hide the modal window after setting the new location data
     $('#locationModal').modal('hide');
     } 
 }
